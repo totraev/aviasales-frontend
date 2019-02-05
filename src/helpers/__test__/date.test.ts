c9f8912e3a4c36c78e980498cc2f49a9842d5334
David Totraev <totraev@gmail.com>
2019-02-06 00:12:41 +0500

    Fix month format, fix logo margins

--- src/helpers/__test__/date.test.ts
+++ src/helpers/__test__/date.test.ts
@@ -2,6 +2,6 @@ import { formatDate } from '../date';
 
 describe('', () => {
   it('Should return formated date', () => {
-    expect(formatDate('07.01.13')).toBe('7 Фев 2013, Чт');
+    expect(formatDate('07.01.13')).toBe('7 фев 2013, Чт');
   });
 });
