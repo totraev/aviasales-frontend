33afb9f549592207387059496860c264b8568041
David Totraev <totraev@gmail.com>
2020-06-20 21:40:20 +0500

    Add mobx folder

---
+++ src/mobx/filterStore.ts
@@ -0,0 +1,33 @@
+import { observable, action } from 'mobx'
+
+export interface ICurrencyMap {
+  eur: number
+  rub: number
+  usd: number
+}
+
+class FilterStore {
+  @observable public currencyMap = new Map<string, number>()
+  @observable public currentCurr = 'rub'
+  @observable public transfers = [true, true, true, true]
+
+  @action public setCurrencies(currencies: ICurrencyMap) {
+    this.currencyMap = new Map(Object.entries(currencies))
+  }
+
+  @action public selectCurrency(curr: string) {
+    this.currentCurr = curr
+  }
+
+  @action public toggleAllCheckboxes(payload: boolean) {
+    this.transfers = this.transfers.map(t => !t)
+  }
+
+  @action public toggleCurrentCheckbox(stops: number, value: boolean) {
+    this.transfers[stops] = value
+  }
+
+  @action public toggleOnlyCurrentCheckbox(stops: number, value: boolean) {
+    this.transfers = this.transfers.map((t, i) => (i === stops ? value : false))
+  }
+}
